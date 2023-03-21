1d17847fd3930c4c43dc1957e7d4f5a8387074d8
Manoj Kumar <manojkumar.pcf@gmail.com>
2023-03-21 19:43:39 +0530

    Initial commit

---
+++ src/main/java/com/example/demospringboot7PM/DemoSpringboot7PmApplication.java
@@ -0,0 +1,13 @@
+package com.example.demospringboot7PM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringboot7PmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringboot7PmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demospringboot7PM/DemoSpringboot7PmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demospringboot7PM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoSpringboot7PmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
