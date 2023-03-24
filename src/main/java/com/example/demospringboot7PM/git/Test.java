3cccba727331cf83590329fd880505e2b2c590d9
Manoj Kumar <manojkumar.pcf@gmail.com>
2023-03-24 20:36:17 +0530

    DEMO COMMIT

---
+++ src/main/java/com/example/demospringboot7PM/git/Test.java
@@ -0,0 +1,9 @@
+package com.example.demospringboot7PM.git;
+
+public class Test {
+
+    public static void main(String[] args) {
+
+        System.out.println("This is for git checkin demo");
+    }
+}
