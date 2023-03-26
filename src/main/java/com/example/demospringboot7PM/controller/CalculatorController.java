088b3f05dfa930b2751dc009ca2a31c80a812f71
Krishna Varshney <yadavseeta168@gmail.com>
2023-03-26 19:17:53 +0530

    Assignment CalculatorController

---
+++ src/main/java/com/example/demospringboot7PM/controller/CalculatorController.java
@@ -0,0 +1,70 @@
+package com.example.demospringboot7PM.controller;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import static com.sun.javafx.fxml.expression.Expression.subtract;
+
+@RestController
+public class CalculatorController {
+@RequestMapping("/addition/{a}/{b}")
+
+    public String add(@PathVariable int a, @PathVariable int b){
+        int c;
+
+        String result ;
+
+        c=a+b;
+
+        result =" Addition of "+a+" and "+b+" = "+c;
+
+            return result;
+    }
+    @RequestMapping("/subtraction/{a}/{b}")
+
+    public String subtract(@PathVariable int a,@PathVariable int b){
+        int c;
+
+        String result;
+
+        c=a-b;
+
+        result = "subtraction of "+a+" and "+b+"= "+c;
+
+        return result;
+    }
+    @RequestMapping("/multiply/{a}/{b}")
+
+    public String multi(@PathVariable int a,@PathVariable int b){
+         int c;
+
+         String result;
+
+         c=a*b;
+
+         result = "multiply of "+a+" and "+b+"="+c;
+
+         return result;
+    }
+    @RequestMapping("/division/{a}/{b}")
+    public String division(@PathVariable int a, @PathVariable int b){
+         int c;
+
+         String result;
+
+         c=a/b;
+
+         result = "division of "+a+" and "+b+" ="+c;
+
+         return result;
+
+
+    }
+
+
+    }
+
+
+
+
