15a7f7a360980724d89f408aea4fbc4f45bebd00
pc user <paramvirsingh127@gmail.com>
2023-03-26 15:25:58 +0530

    demo commit

--- src/main/java/com/example/demospringboot7PM/DemoSpringboot7PmApplication.java
+++ src/main/java/com/example/demospringboot7PM/DemoSpringboot7PmApplication.java
@@ -4,9 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoSpringboot7PmApplication {
+public class  DemoSpringboot7PmApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(DemoSpringboot7PmApplication.class, args);
 	}
 
