905882e7887dbefd3c4588f18874d4cce8121154
Krishna Varshney <yadavseeta168@gmail.com>
2023-03-26 20:26:28 +0530

    Assignment ArmstrongController

---
+++ src/main/java/com/example/demospringboot7PM/controller/ArmstrongController.java
@@ -0,0 +1,43 @@
+package com.example.demospringboot7PM.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ArmstrongController {
+@RequestMapping("/armstrong")
+    public String armstrong(){
+
+        int num =370,temp,digit,res=0;
+
+         temp=num;
+
+         String result="";
+
+         while(num>0) {
+
+             digit = num % 10;
+
+             res = res+ digit * digit * digit;
+
+             num = num / 10;
+         }
+         if(temp==res){
+
+            result= temp+"is Armstrong Number";
+         }
+         else{
+
+             result=temp+"not a Armstrong Number";
+        }
+         return result;
+
+
+
+
+
+
+
+    }
+
+}
