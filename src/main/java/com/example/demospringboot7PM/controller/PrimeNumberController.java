12b36656f15b77ddaf3b83cc00b86bd1963b7316
Krishna Varshney <yadavseeta168@gmail.com>
2023-03-28 01:21:17 +0530

    Assignment Even Number Controller

---
+++ src/main/java/com/example/demospringboot7PM/controller/PrimeNumberController.java
@@ -0,0 +1,33 @@
+package com.example.demospringboot7PM.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class PrimeNumberController {
+    @RequestMapping("/Prime_number")
+    public String Prime() {
+
+        int num = 28, i = 2;
+        boolean flag = true;
+        String result = "";
+
+        while (i < num / 2) {
+            if (num % i == 0) {
+                flag = false;
+                break;
+            } else {
+                flag = true;
+            }
+            if (flag = true) {
+
+                result = "Number is Prime number";
+            } else {
+
+                result = "Number is not Prime number";
+            }
+            i++;
+        }
+        return result;
+    }
+}
