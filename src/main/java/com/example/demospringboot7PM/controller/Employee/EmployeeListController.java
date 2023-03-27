3c3d5e0171e9ddf885be859ee562b2e323245d48
Krishna Varshney <yadavseeta168@gmail.com>
2023-03-27 19:45:55 +0530

    Assignment Employee ArrayList Controller

--- src/main/java/com/example/demospringboot7PM/controller/Employee/EmployeeListController.java
+++ src/main/java/com/example/demospringboot7PM/controller/Employee/EmployeeListController.java
@@ -3,6 +3,8 @@ package com.example.demospringboot7PM.controller.Employee;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+
 @RestController
 public class EmployeeListController {
 @RequestMapping("/Employee")
@@ -15,15 +17,28 @@ public class EmployeeListController {
 
 
     }
+    @RequestMapping("/Emp")
+
+    public ArrayList<Employee> getEmployee(){
+
+         ArrayList<Employee> Emp = new ArrayList<>();
+
+         Employee emp1 = new Employee("Mohit",32,"Agra","IT");
+         Employee emp2 = new Employee("Rahul",30,"Mumbai","CS");
+         Employee emp3 = new Employee("Shekhar",42,"Pune","IT");
+         Employee emp4 = new Employee("Seeta",28,"Tudla","Engineer");
+
+         Emp.add(emp1);
+         Emp.add(emp2);
+         Emp.add(emp3);
+         Emp.add(emp4);
+
+         return Emp;
 
 
 
 
-
-
-
-
-
+    }
 
 
 }
