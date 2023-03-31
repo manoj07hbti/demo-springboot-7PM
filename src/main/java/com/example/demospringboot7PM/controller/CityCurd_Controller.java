1470752d2826bbc866d4feef77e14a4c6ede0298
Krishna Varshney <yadavseeta168@gmail.com>
2023-03-31 19:52:24 +0530

    Assignment City / State curd Controller day 4

--- src/main/java/com/example/demospringboot7PM/controller/CityCurd_Controller.java
+++ src/main/java/com/example/demospringboot7PM/controller/CityCurd_Controller.java
@@ -2,6 +2,7 @@ package com.example.demospringboot7PM.controller;
 
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 
@@ -21,6 +22,29 @@ import java.util.ArrayList;
         state.add(city);
 
          return "state is add to successfully :"+city;
+
+     }
+     @RequestMapping("/get_State")
+     public ArrayList<String> getState(){
+
+         return state;
+     }
+     @RequestMapping("/update_state/{index}/{city}")
+     public String UpdateState(@PathVariable int index ,@PathVariable String city){
+
+         state.set(index,city);
+
+         return "State name as Update :" +city;
+
+     }
+     @RequestMapping("/delete_state/")
+     public String delete_state( @RequestParam int index){
+
+         state.remove(index);
+
+         return "State delete successfully :";
+
+
 
 
      }
