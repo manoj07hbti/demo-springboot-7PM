dfecd1b5b26c2cd5c9fe9cb8066fbeaea687064c
RockAmbika <123267967+Shekhar161706@users.noreply.github.com>
2023-04-04 02:01:12 +0530

    Employee Crud Controller Testing..

---
+++ src/main/java/com/example/demospringboot7PM/controller/EmployeeCrudController.java
@@ -0,0 +1,42 @@
+package com.example.demospringboot7PM.controller;
+
+import com.example.demospringboot7PM.employee.Employee;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+
+@RestController
+public class EmployeeCrudController {
+    ArrayList<Employee> employees = new ArrayList<>();
+
+    @PostMapping("/employee_request")
+    public String addemployeerequest(@RequestBody Employee employee) {
+        employees.add(employee);
+        return "employee detail  added sucessesfull";
+    }
+
+    @PostMapping("/add_employee/{name}/{age}/{department}")
+    public String employees(@PathVariable String name, @PathVariable int age, @PathVariable String department) {
+        Employee employee = new Employee(name, age, department);
+        employees.add(employee);
+        return "employee detail added sucssesfull";
+    }
+
+    @GetMapping("/get_employee")
+    public ArrayList<Employee> get_employee() {
+        return employees;
+    }
+    @PutMapping("/update/{index}/{name}")
+    public String update_data(@PathVariable int index, @PathVariable String name) {
+
+        Employee employee =employees.get(index);
+        employee.setName(name);
+        return "name update sucessesfully"+name;
+    }
+@DeleteMapping("/delete/{index}")
+    public String delete(@PathVariable int index){
+        employees.remove(index);
+        return "employee remove sucessesfull";
+
+    }
+}
---
+++ src/main/java/com/example/demospringboot7PM/employee/Employee.java
@@ -0,0 +1,38 @@
+package com.example.demospringboot7PM.employee;
+
+public class Employee {
+
+    String name;
+    int age;
+    String department;
+
+    public Employee(String name, int age, String department) {
+        this.name = name;
+        this.age = age;
+        this.department = department;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+}
