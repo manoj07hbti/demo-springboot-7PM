f90ada104a448411ebf4d2701fcb826ba55a0640
ASHISH <vermaashish732@gmail.com>
2023-04-08 02:00:16 +0530

    student crud controller + POSTMAN

--- src/main/java/com/example/demospringboot7PM/controller/StudentCrudControllerRequest_PostMapping.java
+++ src/main/java/com/example/demospringboot7PM/controller/StudentCrudControllerRequest_PostMapping.java
@@ -6,26 +6,25 @@ import org.springframework.web.bind.annotation.*;
 import java.util.ArrayList;
 
 @RestController
-public class StudentCrudController {
+public class StudentCrudControllerRequest_PostMapping {
 
     ArrayList<Student> students= new ArrayList<>();
 
-    @PostMapping("/add_student/{name}/{age}/{section}")
-    public String addStudent(@PathVariable String name , @PathVariable int age, @PathVariable String section){
+    @PostMapping("/add_student_postmapping")
+    public String addStudent(@RequestBody Student student){
 
         // creating data of student
-        Student student= new Student(name,age,section);
         students.add(student);
 
         return "Student details added successfully";
     }
 
-    @GetMapping("/get_students")
+    @GetMapping("/get_students_postmapping")
     public ArrayList<Student>  getStudents(){
 
         return students;
     }
-    @PutMapping("/update/{index}/{name}")
+    @PutMapping("/update_postmapping/{index}/{name}")
     public String updateStudent(@PathVariable int index,@PathVariable String name){
 
         Student student=students.get(index);
@@ -33,7 +32,7 @@ public class StudentCrudController {
         return "Name updated successfully "+name;
     }
 
-    @DeleteMapping("/delete_student/{index}")
+    @DeleteMapping("/delete_student_postmapping/{index}")
     public String delete(@PathVariable int index){
 
         students.remove(index);
