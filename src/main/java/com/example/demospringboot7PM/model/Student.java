56ab3749ad9c78a371ba4701e75aac080b3a012b
Manoj Kumar <manojkumar.pcf@gmail.com>
2023-04-08 19:34:52 +0530

    DB connection

--- src/main/java/com/example/demospringboot7PM/model/Student.java
+++ src/main/java/com/example/demospringboot7PM/model/Student.java
@@ -1,9 +1,25 @@
-package com.example.demospringboot7PM.student;
+package com.example.demospringboot7PM.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity(name="STUDENT_7PM")
 public class Student {
+
+    @Id // primary key and it will be unique
+    @GeneratedValue // it will automatically generate id for every record
+    long id;
     String name;
     int age;
+
+    @Column(name="stream")
     String section;
+
+    //default constructor
+    public Student() {
+    }
 
     public Student(String name, int age, String section) {
         this.name = name;
